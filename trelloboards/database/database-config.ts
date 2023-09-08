575cefde92112aff698337a529241d8e574d4112
Caio Augusto Palma <caio.palma@fatec.sp.gov.br>
2023-09-08 02:20:14 +0000

    Add database connection, entities and facade impl.

---
+++ trelloboards/database/database-config.ts
@@ -0,0 +1,35 @@
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
+
+const SUPABASE_URL = "https://malmhmbtzhyasmppciyh.supabase.co"
+
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1hbG1obWJ0emh5YXNtcHBjaXloIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTQxMjcyMjUsImV4cCI6MjAwOTcwMzIyNX0.HLUPa8-boKQjzC27iHeORGf_0KBG_6Uh1g7FMRXwHDc'
+
+const options = {
+    //     schema: "public",
+    //     headers: { "x-my-custom-header": "my-app-name" },
+    //     autoRefreshToken: true,
+    //      persistSession: true,
+    //     detectSessionInUrl: true,
+};
+
+export const dbconnection = createClient(SUPABASE_URL, SUPABASE_KEY);
+
+export type Board = {
+    created_at: string
+    id: number
+    name: string | null
+  }
+  
+  export type Item = {
+    created_at: string
+    id: number
+    list_id: number | null
+    message: string | null
+  }
+  
+  export type List = {
+    board_id: number | null
+    created_at: string
+    id: number
+    name: string | null
+  }
--- trelloboards/main.ts
+++ trelloboards/main.ts
@@ -1,4 +1,7 @@
 import { Hono } from 'https://deno.land/x/hono/mod.ts'
+import { daoFacadeImpl } from './database/dao-facade-impl.ts'
+
+console.log(await daoFacadeImpl.find_all_boards())
 
 const app = new Hono()
 
