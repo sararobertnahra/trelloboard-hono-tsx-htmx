445b3c4935d4e0ddd6942519933c207f1c96a908
Caio Augusto Palma <caio.palma@fatec.sp.gov.br>
2023-09-10 17:39:48 +0000

    Fix some issues with list removal and improve ux/ui.

--- trelloboards/view/item-component.tsx
+++ trelloboards/view/item-component.tsx
@@ -12,16 +12,16 @@ export const ItemComponent = (props: { model: AppModel, item: Item }) => {
     return (
         <li class="list-group-item shadow" id={item_id}>
             <div class="row">
-                <div class="col-3 p-1">
+                <div class="col-10 p-2">
+                    <span>{props.item.message}</span>
+                </div>
+                <div class="col-2 p-1">
                     <form hx-delete="/hx/components/removeitem" hx-target={target_component_id} hx-swap="outerHTML">
                         <input type="hidden" name="id" value={props.item.id} />
                         <button class="btn btn-light">
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash-2"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path><line x1="10" y1="11" x2="10" y2="17"></line><line x1="14" y1="11" x2="14" y2="17"></line></svg>
                         </button>
                     </form>
-                </div>
-                <div class="col-9 p-1">
-                    <span>{props.item.message}</span>
                 </div>
             </div>
         </li >
--- trelloboards/view/new-item-component.tsx
+++ trelloboards/view/new-item-component.tsx
@@ -8,15 +8,15 @@ import { List } from '../database/database-config.ts';
 
 export const NewItemComponent = (props: { model: AppModel, list: List}) => {
     return (
-        <form class="card-footer shadow" hx-post="/hx/compoments/addnewitem" hx-swap="outerHTML">
-            <div class="mb-3">
-                <input type="hidden" name="list_id" id="list_id" value={props.list.id} />
+        <form class="card-body row shadow" hx-post="/hx/compoments/addnewitem" hx-swap="outerHTML">
+            <input type="hidden" name="list_id" id="list_id" value={props.list.id} />
+            <div class="col-10 p-2">
+                <input class="form-control mb-2" type="text" name="message" id="message" placeholder="Enter a title for this post..."></input>
             </div>
-            <div class="mb-3">
-                <input class="form-control mb-3" type="text" name="message" id="message" placeholder="Enter a title for this post..."></input>
-            </div>
-            <div class="">
-                <input class="btn btn-primary mb-3" type="submit" value="New Item"/>
+            <div class="col-2 p-1">
+                <button class="btn btn-primary" type="submit">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-plus"><line x1="12" y1="5" x2="12" y2="19"></line><line x1="5" y1="12" x2="19" y2="12"></line></svg>
+                </button>
             </div>
         </form>
     )
